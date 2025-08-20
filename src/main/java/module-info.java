621ca73eb9ec9f9af493afdb3d900daf7a2a50a2
John Raymond Wold <john.raymond.wold@nav.no>
2025-08-20 15:33:35 +0200

    Enkel logging for å verifisere kjøring

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,5 @@
 module no.nav.aareg.abonnering {
+    requires static lombok;
+    requires ch.qos.logback.classic;
+    requires org.slf4j;
 }
--- src/main/java/no/nav/aareg/abonnering/Jobb.java
+++ src/main/java/no/nav/aareg/abonnering/Jobb.java
@@ -1,4 +1,10 @@
 package no.nav.aareg.abonnering;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public final class Jobb {
+    public static void main(final String... argumenter) {
+        log.atInfo().log("Kjørt ok");
+    }
 }
